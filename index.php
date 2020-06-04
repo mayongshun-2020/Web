f8aa792cfb184afcf545c05c182af123f29d9a8a
mayongshun <mayongshun2018@163.com>
2020-06-04 13:30:53 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+<link href="css/style.css" rel="stylesheet"/>
+</head>
+<body>
+<div id="content">
+ <!--top-->
+    <div class="top">
+        <img src="images/img1.png"/>
+    </div>
+    <div class="button">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Brands</a></li>
+            <li><a href="#">Storys</a></li>
+            <li><a href="#">Historys</a></li>
+            <li><a href="#">About Me</a></li>
+        </ul>
+    </div>
+    <!--top-->
+    <!--banner-->
+    <div class="banner">
+        <img src="images/img2.png" alt="banner">
+        <img src="images/img3.png" alt="banner">
+        <img src="images/img4.png" alt="banner">
+        <ul class="btn">
+            <li class="hot"></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <!--banner-->
+    <!--desc-->
+    <div class="desc">
+        <img src="images/img5.png" alt="" class="logo">
+        <div class="cont">
+            <div class="box">
+                <h2>Event Descrption</h2>
+                <p>A Look at Spring 2018's Attention-Grabbing Runways Technology Is Coming for the Fashion Industry—​Whether They Like It or Not For the 24th year, ELLE's Women in Hollywood issue highlights the biggest names in the industry right now—and delves deeper into these women's stories. </p>
+                <a href="#">ReadMore</a>
+            </div>
+
+
+        </div>
+    </div>
+    <!--desc-->
+    <!--news-->
+    <div class="news">
+        <img src="images/img7.png" alt="" class="logo">
+        <div class="newsbox">
+            <div class="newsub">
+                <img src="images/img8.png" alt="">
+                <h3>A Look at Spring 2018</h3>
+                <p>View Times:16:39</p>
+                <p>Published date:2019-04-01</p>
+                <p>Author:cbdyma</p>
+                <p>Description:</p>
+                <p>Attention-Grabbing Runways Technology Is Coming for the Fashion Industry—​Whether They Like It or Not For the 24th year, ELLE's Women</p>
+
+            </div>
+
+            <div class="newsub">
+                <img src="images/img9.png" alt="">
+                <h3>A Look at Spring 2018</h3>
+                <p>View Times:16:39</p>
+                <p>Published date:2019-04-01</p>
+                <p>Author:cbdyma</p>
+                <p>Description:</p>
+                <p>Attention-Grabbing Runways Technology Is Coming for the Fashion Industry—​Whether They Like It or Not For the 24th year, ELLE's Women</p>
+
+            </div>
+
+            <div class="newsub">
+                <img src="images/img10.png" alt="">
+                <h3>A Look at Spring 2018</h3>
+                <p>View Times:16:39</p>
+                <p>Published date:2019-04-01</p>
+                <p>Author:cbdyma</p>
+                <p>Description:</p>
+                <p>Attention-Grabbing Runways Technology Is Coming for the Fashion Industry—​Whether They Like It or Not For the 24th year, ELLE's Women</p>
+
+            </div>
+
+            <div class="newsub">
+                <img src="images/img11.png" alt="">
+                <h3>A Look at Spring 2018</h3>
+                <p>View Times:16:39</p>
+                <p>Published date:2019-04-01</p>
+                <p>Author:cbdyma</p>
+                <p>Description:</p>
+                <p>Attention-Grabbing Runways Technology Is Coming for the Fashion Industry—​Whether They Like It or Not For the 24th year, ELLE's Women</p>
+
+            </div>
+        </div>
+    </div>
+    <!--news-->
+    <!--online-->
+    <div class="online">
+        <img src="images/img12.png" alt="" class="logo">
+        <div class="onbox">
+        <h2>Sponsors Online exhibition</h2>
+            <div class="txtCon">
+                <p>Vehicle Name:Ford</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon1.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fbbb</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon2.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fccc</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon3.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fddd</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon4.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Feee</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon1.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Ffff</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon2.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:ggg</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon3.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fhhh</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon4.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fiii</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon1.png" alt=""></p>
+            </div>
+            <div class="txtCon">
+                <p>Vehicle Name:Fjjj</p>
+                <p>Display Type:special release </p>
+                <p>Company Name:abc</p>
+                <p>Vehicle Model:<img src="images/icon2.png" alt=""></p>
+            </div>
+            <ul class="icon">
+                <li class="hot"><img src="images/car1.png" alt=""></li>
+                <li><img src="images/car2.png" alt=""></li>
+                <li><img src="images/car3.png" alt=""></li>
+                <li><img src="images/car4.png" alt=""></li>
+                <li><img src="images/car5.png" alt=""></li>
+                <li><img src="images/car6.png" alt=""></li>
+                <li><img src="images/car7.png" alt=""></li>
+                <li><img src="images/car8.png" alt=""></li>
+                <li><img src="images/car9.png" alt=""></li>
+                <li><img src="images/car10.png" alt=""></li>
+            </ul>
+        </div>
+
+    </div>
+    <!--online-->
+    <!--ticket-->
+    <div class="ticket">
+        <img src="images/img14.png" alt="" class="logo"/>
+        <div class="tickcon">
+            <form action="">
+                <h2>Tickets Service</h2>
+                <p><label>Applicant Name</label><input type="text"></p>
+                <p><label>Visiting Date</label><input type="text"></p>
+                <p><label>Contact information</label><input type="textarea"></p>
+                <p><label>Attendees number</label><input type="text"></p>
+                <p><label>Application Time</label><input type="text"></p>
+                <p><input type="submit" value="Submit" class="btn"><input type="button" value="Cancel" class="btn b2"></p>
+            </form>
+        </div>
+    </div>
+    <!--ticket-->
+    <!--map-->
+    <div class="map">
+        <img src="images/img16.png" alt="" class="logo">
+        <div class="mapbox">
+            <div class="left">
+                <img src="images/img17.png" alt="">
+            </div>
+            <div class="right">
+                <div class="time">
+                    <span class="timespan"></span>
+                </div>
+                <p><img src="images/img19.png" alt=""></p>
+            </div>
+        </div>
+    </div>
+    <!--map-->
+    <!--footer-->
+    <div class="footer">
+     <div class="logo"><img src="images/img21.png" alt=""></div>
+     <div class="copy">
+            <div class="button">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Brands</a></li>
+                    <li><a href="#">Storys</a></li>
+                    <li><a href="#">Historys</a></li>
+                    <li><a href="#">About Me</a></li>
+                </ul>
+            </div>
+            <p>CopyRight2016xxx.AllrightReserved</p>
+     </div>
+        <div class="seo"><img src="images/img22.png" alt=""></div>
+    </div>
+    <!--footer-->
+    <!--silder start-->
+    <div class="silder">
+        <div class="best"><a href="#"><img src="images/logo-s.png"/></a></div>
+        <div class="box"></div>
+    </div>
+    <!--silder end-->
+</div>
+
+<script src="js/jquery-3.2.1.min.js"></script>
+<script>
+    //轮播：
+    var _index=0;
+   $("ul.btn li").hover(function(){
+      $(this).addClass("hot").siblings().removeClass("hot");
+      _index=$(this).index();
+      $(".banner img").eq(_index).show().siblings("img").hide();
+   },function(){});
+   //在线
+    $("ul.icon li").hover(function(){
+        $(this).addClass("hot").siblings().removeClass("hot");
+        _index=$(this).index();
+        $(".txtCon").eq(_index).show().siblings("div").hide();
+    },function(){});
+
+    //侧边栏
+    var mark=1;
+    $('.best a').click(function(){
+        if(mark==1){
+            $('.silder').animate({width:'260'},500);
+            mark=0;
+        }else{
+            $('.silder').animate({width:'35'},500);
+            mark=1;
+        }
+    });
+    //倒计时
+    var starttime = new Date("2020/1/11");
+    setInterval(function () {
+        var nowtime = new Date();
+        var time = starttime - nowtime;
+        var day = parseInt(time / 1000 / 60 / 60 / 24);
+        var hour = parseInt(time / 1000 / 60 / 60 % 24);
+        var minute = parseInt(time / 1000 / 60 % 60);
+        var seconds = parseInt(time / 1000 % 60);
+        $('.timespan').html(day + "天" + hour + "小时" + minute + "分钟" + seconds + "秒");
+    }, 1000);
+</script>
+</body>
+</html>
